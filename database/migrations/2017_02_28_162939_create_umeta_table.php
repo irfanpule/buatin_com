c2f58deb415a78fff26c4001a25fe9b90715e578
irfanpule <irfan.pule2@gmail.com>
2017-03-01 17:57:27 +0700

    add table umeta

--- database/migrations/2017_02_28_162939_create_umeta_table.php
+++ database/migrations/2017_02_28_162939_create_umeta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUmetaTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('umeta', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique()->nullable();
-            $table->string('password')->nullable();
-            $table->string('provider');
-            $table->string('provider_id')->unique();
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->string('meta_key', 255);
+            $table->string('meta_value',255);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('umeta');
     }
 }
