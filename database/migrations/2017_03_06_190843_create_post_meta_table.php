f6d0479ac5cdd0cc71bdde747c687511d04f6d91
irfanpule <irfan.pule2@gmail.com>
2017-03-07 04:41:39 +0700

    add post meta table

---
+++ database/migrations/2017_03_06_190843_create_post_meta_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_meta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id')->unsigned();
+            $table->string('meta_key', 100);
+            $table->string('meta_value');
+            $table->timestamps();
+
+            $table->foreign('post_id')
+                    ->references('id')
+                    ->on('posts')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_meta');
+    }
+}
