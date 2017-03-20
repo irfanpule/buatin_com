a986a57bae1daef6948acfae510d40cc8e2b55a6
irfanpule <irfan.pule2@gmail.com>
2017-03-20 19:21:47 +0700

    add concept form pengaturan

--- resources/views/contents/pengaturan.blade.php
+++ resources/views/contents/pengaturan.blade.php
@@ -9,8 +9,24 @@
                 <div class="panel-heading">Pengaturan</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    <div>
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#profil" aria-controls="profil" role="tab" data-toggle="tab">Profil</a></li>
+                            <li role="presentation"><a href="#ubah-password" aria-controls="ubah-password" role="tab" data-toggle="tab">Ubah Password</a></li>
+                            <li role="presentation"><a href="#email" aria-controls="email" role="tab" data-toggle="tab">Email</a></li>
+                        </ul>
+
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="profil"> @include('forms.profile') </div>
+                            <div role="tabpanel" class="tab-pane" id="ubah-password"> @include('forms.change-password') </div>
+                            <div role="tabpanel" class="tab-pane" id="email">...</div>
+                        </div>
+                    </div>
                 </div>
+
+                
             </div>
         </div>
     </div>
---
+++ resources/views/forms/change-password.blade.php
@@ -0,0 +1,12 @@
+<form>
+    <div class="form-group">
+        <label for="InputPasswordLama">Password Lama</label>
+        <input type="email" class="form-control" id="InputPasswordLama" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label for="InputPasswordBaru">Password Baru</label>
+        <input type="password" class="form-control" id="InputPasswordBaru" placeholder="Password">
+    </div>
+    
+    <button type="submit" class="btn btn-default">Submit</button>
+</form>
