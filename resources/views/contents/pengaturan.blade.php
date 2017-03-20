ec841f53e3fe2e843bbd4ff5970aa601048ff5d5
irfanpule <irfan.pule2@gmail.com>
2017-03-20 18:43:51 +0700

    ubah struk folder view dan template

--- resources/views/contents/pengaturan.blade.php
+++ resources/views/contents/pengaturan.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
-@section('title', 'Dashboard')
+@section('title', 'Pengaturan')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Pengaturan</div>
 
                 <div class="panel-body">
                     You are logged in!
