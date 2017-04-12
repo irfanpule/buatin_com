d038ab1f591efeeb8dfab32bef719b643a602b95
irfanpule <irfan.pule2@gmail.com>
2017-04-12 15:49:20 +0700

    edit form portfolio

--- resources/views/contents/add-portfolio.blade.php
+++ resources/views/contents/add-portfolio.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 @section('title', 'Portofolio')
-@section('css')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.css" rel="stylesheet">
-@endsection
 
 @section('content')
 <div class="container">
@@ -52,5 +49,39 @@
             });
         });
         
+    </script>
+
+    {{-- rupiah type --}}
+    <script>
+        
+        $('.input-rupiah').keyup(function(){
+            var v = formatRupiah($(this).val());
+            $(this).val(v);
+        })
+
+        /* Fungsi */
+        function formatRupiah(angka, prefix)
+        {
+            var number_string = angka.replace(/[^,\d]/g, '').toString(),
+                split	= number_string.split(','),
+                sisa 	= split[0].length % 3,
+                rupiah 	= split[0].substr(0, sisa),
+                ribuan 	= split[0].substr(sisa).match(/\d{3}/gi);
+                
+            if (ribuan) {
+                separator = sisa ? '.' : '';
+                rupiah += separator + ribuan.join('.');
+            }
+            
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+        }
+
+        $('#PriceStart').keyup(function() {
+            var value = $( this ).val();
+            $( "p" ).text( value );
+        }).keyup();
+
+  
     </script>
 @endsection
--- resources/views/forms/portfolio.blade.php
+++ resources/views/forms/portfolio.blade.php
@@ -25,7 +25,7 @@
 
             <div class="form-group">
                 {{ Form::label('InputDescription', 'Deskripsi')}}
-                {{ Form::textarea('post_title',null, ['class' => 'form-control', 'placeholder' => 'Tuliskan tentang Produk/Proyek/Karya lebih detail']) }}
+                {{ Form::textarea('post_content',null, ['class' => 'form-control', 'placeholder' => 'Tuliskan tentang Produk/Proyek/Karya lebih detail']) }}
             </div>
         </div>
  
@@ -36,7 +36,7 @@
         @for($i = 1; $i <= 6; $i++)
             <div class="col-md-2 col-xs-6">
                 <div class="img-portfolio" no-data="{{$i}}" id="imgUpload{{$i}}"><i class="fa fa-camera fa-3x"></i> foto {{$i}}</div>
-                <input type="file" name="image{{$i}}" id="chooseFile{{$i}}" style="display:none;">
+                <input type="file" name="image{{$i}}" id="chooseFile{{$i}}" accept="image/gif, image/jpeg, image/png" style="display:none;">
 
                 <label id="imgTumbh{{$i}}" style="display:none;">
                     Default
@@ -58,7 +58,7 @@
             <div class="form-group">
                 <div class="input-group">
                     <div class="input-group-addon">Rp.</div>
-                    {{ Form::text('price_start',null, ['class' => 'form-control', 'placeholder' => '100.000']) }}
+                    {{ Form::text('price_start',null, ['class' => 'form-control', 'id'=>'PriceStart', 'placeholder' => '100.000']) }}
                 </div>
             </div>
         </div>
@@ -67,13 +67,14 @@
             <div class="form-group">
                 <div class="input-group">
                     <div class="input-group-addon">Rp.</div>
-                    {{ Form::text('price_end',null, ['class' => 'form-control', 'placeholder' => '500.000']) }}
+                    {{ Form::text('price_end',null, ['class' => 'form-control', 'id'=>'PriceEnd', 'placeholder' => '500.000']) }}
                 </div>
             </div>
         </div>
 
         
         <div class="col-md-12">
+        <p class="input-rupiah"></p>
         <hr>
             <button type="submit" class="btn btn-primary">Simpan</button>
         </div>
