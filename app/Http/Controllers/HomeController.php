9b152fae2b6ae94afb7aace1edd9d72298d53ca8
irfanpule <irfan.pule2@gmail.com>
2017-04-21 05:07:08 +0700

    edit : welcome page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
+use Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
     /**
      * Show the application dashboard.
@@ -23,6 +16,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        /*
+            show all post pagination 10
+        */
+        
+        $posts = Post::with('user', 'post_metas')->orderBy('created_at', 'desc')->paginate(9); 
+        return view('welcome', compact('posts'));
+    }
+
+    public function home()
+    {
+        /*
+            show all post pagination 10
+        */
+        
+        $posts = Post::paginate(10); 
+        return view('home', compact('posts'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,37 @@
 
 
 @section('content')
-<div class="flex-center position-ref full-height">
+<div class="flex-center position-ref full-height" style="background-image:url('img/office-gadget.jpg');">
     
     <div class="content">
-        <div class="title m-b-md">
-            Laravel
+    
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="title title-style">
+                        <b>Selamat Datang di Buatin</b>
+                    </div>
+                    <div class="title-small title-style m-b-md">
+                        Temukan orang yang dapat membuat konsepmu menjadi nyata
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <h3 class="title-style">Anda ingin dibuatkan apa ?</h3>
+                    <form class="form-inline">
+                        <div class="form-group">
+                            {{ Form::select('category', ['pilih']  , null, ['class' => 'form-control input-lg', 'placeholder' => 'pilih Kategori']) }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::text('key',null, ['class' => 'form-control input-lg', 'placeholder' => 'Cari']) }}
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-lg"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+
+            </div>
         </div>
 
-        <div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
-        </div>
     </div>
 </div>
 
