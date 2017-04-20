4c32d1934e9ca7a46980a9a6df8de0ad42b15e1b
irfanpule <irfan.pule2@gmail.com>
2017-04-21 05:09:22 +0700

    change: css image default

--- resources/views/includes/card-list.blade.php
+++ resources/views/includes/card-list.blade.php
@@ -3,18 +3,24 @@
     @foreach($posts as $post)    
         <div class="col-sm-4 col-lg-4 col-md-4">
             <div class="panel panel-default">
+
+                @foreach($post->post_metas->where('meta_key', 'default_image') as $meta)
+                    @php $img = $meta->meta_value @endphp
+                @endforeach
+                <div class="img-thumb" style="background-image:url('{{ asset($img) }}');">
+                </div>
+
+                
                 <div class="panel-heading">
                     <h4><a href="#">{{ $post->post_title }}</a></h4>
-
                 </div>
 
                 <div class="panel-body">
-                    <img src="http://placehold.it/320x150" alt="">
 
                     <div class="caption">
                         <h4>Rp. <span class="rupiah">{{ $post->price_start }}</span> - <span class="rupiah">{{ $post->price_end }}</span> </h4>   
                     </div>
-
+                    
                     <span>{{ $post->user->display_name }} </span>
                 </div>
 
